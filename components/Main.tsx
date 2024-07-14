3a746fa2652613e43795ae62a98f03d682bbcef5
palakkeni5 <palakkeni@gmail.com>
2024-07-14 16:23:30 -0400

    changed background image and removed comments

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-// import 'react-native-gesture-handler';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -7,8 +6,6 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {FullImage} from "./FullImage";
 import {HomeScreen} from "./Home";
 import {ImageLists} from "./ImageLists";
-// import FullCatogeryScreen from "./components/FullScreen"
-// import ImageDisplay from "./components/ImageDisplay"
 
 const Stack = createNativeStackNavigator();
 
