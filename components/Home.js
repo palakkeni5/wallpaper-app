f8d3d49db4b49dbb683ae224e567ab51d955af44
palakkeni5 <palakkeni@gmail.com>
2024-07-14 16:29:17 -0400

    modified headline

--- components/Home.js
+++ components/Home.js
@@ -132,7 +132,7 @@ export const HomeScreen = ({navigation}) => {
               </Text>
               <Text style={{fontSize: 18, fontWeight: 'bold', color: '#FFF'}}>
                 {' '}
-                Quality Wallpaper's{' '}
+                Quality Wallpapes{' '}
               </Text>
             </Animated.View>
 
