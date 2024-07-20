720b942b72a56bea9fe18f1257fdf1504baed7f2
palakkeni5 <palakkeni@gmail.com>
2024-07-20 15:57:40 -0400

    renamed packages successfully

--- android/app/src/main/java/com/palakkeni5/wallpaper/app/MainActivity.kt
+++ android/app/src/main/java/com/palakkeni5/wallpaper/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.wallpaperapp
+package com.palakkeni5.wallpaper.app
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/palakkeni5/wallpaper/app/MainApplication.kt
+++ android/app/src/main/java/com/palakkeni5/wallpaper/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.wallpaperapp
+package com.palakkeni5.wallpaper.app
 
 import android.app.Application
 import com.facebook.react.PackageList
